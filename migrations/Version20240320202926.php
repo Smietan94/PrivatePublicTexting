731c9a26a3dc68ffb151af55c5df2c3174fa318a
Smietan94 <kkrzysiek101@gmail.com>
2024-03-21 13:46:44 +0100

    migration update

--- migrations/Version20240320202926.php
+++ migrations/Version20240320202926.php
@@ -20,12 +20,12 @@ final class Version20240320202926 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE conversations ADD last_message_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE conversations ADD CONSTRAINT FK_C2521BF1BA0E79C3 FOREIGN KEY (last_message_id) REFERENCES messages (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_C2521BF1BA0E79C3 ON conversations (last_message_id)');
-        $this->addSql('ALTER TABLE messages ADD conversation_id INT NOT NULL');
-        $this->addSql('ALTER TABLE messages ADD CONSTRAINT FK_DB021E969AC0396 FOREIGN KEY (conversation_id) REFERENCES conversations (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE INDEX IDX_DB021E969AC0396 ON messages (conversation_id)');
+        // $this->addSql('ALTER TABLE conversations ADD last_message_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE conversations ADD CONSTRAINT FK_C2521BF1BA0E79C3 FOREIGN KEY (last_message_id) REFERENCES messages (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        // $this->addSql('CREATE UNIQUE INDEX UNIQ_C2521BF1BA0E79C3 ON conversations (last_message_id)');
+        // $this->addSql('ALTER TABLE messages ADD conversation_id INT NOT NULL');
+        // $this->addSql('ALTER TABLE messages ADD CONSTRAINT FK_DB021E969AC0396 FOREIGN KEY (conversation_id) REFERENCES conversations (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        // $this->addSql('CREATE INDEX IDX_DB021E969AC0396 ON messages (conversation_id)');
     }
 
     public function down(Schema $schema): void
